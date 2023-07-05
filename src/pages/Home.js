dd3fe6516119a38205a99f8b8ca702dff86da206
Filipe Ferreira <filipeferreira94@gmail.com>
2023-07-05 09:39:10 +0100

    fix(home): removed unused states.
    timerTotal not being used.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,13 @@
-import { Container, Flex } from '@mantine/core';
+import { Flex } from '@mantine/core';
 import ToDoList from '../components/ToDoList';
 import Spotify from '../components/Spotify';
 import { useEffect, useState } from 'react';
 import settingsService from '../services/settings.service';
 import PomodoroControls from '../components/Pomodoro/PomodoroControls';
-import TimerBar from '../components/Pomodoro/TimerBar';
 import CowSay from '../components/CowSay';
 
 export default function Home({ timerService }) {
 	// console.log("timerService props in Home", props.timerService);
-
-	const [timerTotal, setTimerTotal] = useState(0);
 	const [settings, setSettings] = useState({});
 
 	useEffect(() => {
@@ -39,10 +36,7 @@ export default function Home({ timerService }) {
 			direction="row"
 			wrap="wrap"
 		>
-			<PomodoroControls
-				timerService={timerService}
-				timers={settings?.timers}
-			/>
+			<PomodoroControls timerService={timerService} timers={settings?.timers} />
 			<ToDoList />
 			<Spotify contentUrl={settings?.spotifyContent} />
 			<CowSay />
