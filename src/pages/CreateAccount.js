6b18474ca9b346e49eb009585a93c03009cc0093
Filipe Ferreira <filipeferreira94@gmail.com>
2023-07-12 09:23:33 +0100

    minor(createAccount): minor style changes to margins and font weight

--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -6,6 +6,8 @@ import {
 	Text,
 	Container,
 	Button,
+	Group,
+	Image,
 } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { useNavigate, Link } from 'react-router-dom';
@@ -49,59 +51,95 @@ export default function CreateAccount() {
 			});
 	};
 
+	const size = 50;
 	return (
-		<Container size={520} my={60}>
-			<form onSubmit={signUp}>
-				<Title
-					align="center"
-					sx={(theme) => ({
-						fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-						fontWeight: 900,
-					})}
-				>
-					Join us!
-				</Title>
+		<>
+			<div style={{ display: 'flex', justifyContent: 'center' }}>
+				<Group spacing={5} mt={0} align="center">
+					<Image
+						width={size}
+						height={size}
+						src="./android-chrome-512x512.png"
+					/>
+					<Text
+						mr={20}
+						sx={(theme) => ({
+							fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+							fontWeight: 900,
+							fontSize: size,
+						})}
+					>
+						Focus Dashboard
+					</Text>
+				</Group>
+			</div>
+			<div style={{ display: 'flex', justifyContent: 'center' }}>
+				<Group spacing={5} align="center">
+					<Text
+						mr={20}
+						sx={(theme) => ({
+							fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+							fontSize: 16,
+						})}
+						color="grey"
+					>
+						All you need to get your work done.
+					</Text>
+				</Group>
+			</div>
+			<Container size={520} my={60}>
+				<form onSubmit={signUp}>
+					<Title
+						align="center"
+						sx={(theme) => ({
+							fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+							fontWeight: 500,
+						})}
+					>
+						Join us!
+					</Title>
 
-				<Text color="dimmed" size="sm" align="center" mt={5}>
-					Already have an account?{' '}
-					<Link style={{ textDecoration: 'none' }} to="/login">
-						Login
-					</Link>
-				</Text>
-				<Paper shadow="md" p={30} mt={30} radius="md">
-					{errorMessage && (
-						<Text size="sm" color="red">
-							{errorMessage}
-						</Text>
-					)}
-					<TextInput
-						label="Email"
-						name="email"
-						type="email"
-						placeholder="your@email.com"
-						{...form.getInputProps('email')}
-						required
-					/>
-					<TextInput
-						label="Name"
-						name="username"
-						placeholder="Your name"
-						{...form.getInputProps('name')}
-						required
-					/>
-					<PasswordInput
-						label="Password"
-						placeholder="Your password"
-						{...form.getInputProps('password')}
-						required
-						mt="md"
-					/>
+					<Text color="dimmed" size="sm" align="center" mt={5}>
+						Already have an account?{' '}
+						<Link style={{ textDecoration: 'none' }} to="/login">
+							Login
+						</Link>
+					</Text>
+					<Paper shadow="md" p={30} mt={30} radius="md">
+						{errorMessage && (
+							<Text size="sm" color="red">
+								{errorMessage}
+							</Text>
+						)}
+						<TextInput
+							label="Email"
+							name="email"
+							type="email"
+							placeholder="your@email.com"
+							{...form.getInputProps('email')}
+							required
+						/>
+						<TextInput
+							label="Name"
+							name="username"
+							placeholder="Your name"
+							{...form.getInputProps('name')}
+							required
+						/>
+						<PasswordInput
+							label="Password"
+							placeholder="Your password"
+							{...form.getInputProps('password')}
+							required
+							mt="md"
+						/>
 
-					<Button fullWidth mt="xl" type="submit">
-						Register
-					</Button>
-				</Paper>
-			</form>
-		</Container>
+						<Button fullWidth mt="xl" type="submit">
+							Register
+						</Button>
+					</Paper>
+				</form>
+			</Container>
+		</>
 	);
 }
